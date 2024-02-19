01f3cba3c3c8676cc276ad378bb07f74387b12da
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-02-19 13:43:41 +0600

    Add transformation type page and header component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+import { Header } from '@/components/shared';
+import { transformationTypes } from '@/constants';
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!userId) redirect('/sign-in');
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,2 +1,3 @@
 export { default as MobileNav } from '@/components/shared/MobileNav';
 export { default as Sidebar } from '@/components/shared/Sidebar';
+export { default as Header } from '@/components/shared/Header';
